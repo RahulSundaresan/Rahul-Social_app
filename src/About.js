f3b2218f5b641a9b74d753fe729cc51335d48f9d
RahulSundaresan <rahulsundaresan.18@gmail.com>
2023-07-28 14:41:17 +0530

    changes made

--- src/About.js
+++ src/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <main className='About'>
-      <h2>About</h2>
+      <h2>About Us</h2>
       <p>Hi,I am Rahul Sundareasan.Enjoy your life</p>
     </main>
   )
