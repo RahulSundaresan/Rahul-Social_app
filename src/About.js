20d9d856d3a1e0164f00b828ba87dafeadf4ed6d
RahulSundaresan <rahulsundaresan.18@gmail.com>
2023-07-28 14:36:51 +0530

    first-commit

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <main className='About'>
+      <h2>About</h2>
+      <p>Hi,I am Rahul Sundareasan.Enjoy your life</p>
+    </main>
+  )
+}
+
+export default About
---
+++ src/PostPage.js
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react'
+import { useParams, Link } from 'react-router-dom'
+import DataContext from './context/DataContext';
+
+const PostPage = () => {
+  const { posts, handleDelete }=useContext(DataContext)
+  const { id } = useParams();
+  const post = posts.find(post => (post.id).toString() == id);
+  return (
+    <main className='PostPage'>
+      <article className='post'>
+        {post &&
+          <>
+            <h2>{post.title}</h2>
+            <p className='postDate'>{post.dateTime}</p>
+            <p className='postBody'>{post.body}</p>
+            <Link to={`/edit/${post.id}`}><button className='editbutton' >Edit Post</button></Link>
+            <button className='deletebutton' onClick={() => handleDelete(post.id)}>Delete Post</button>
+          </>
+        }
+        {
+          !post &&
+          <>
+            <h2>Post Not Found</h2>
+            <Link to='/'>Visit Home page</Link>
+          </>
+        }
+
+      </article>
+    </main>
+  )
+}
+
+export default PostPage
